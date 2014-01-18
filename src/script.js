7fd59c0845785b6d8aa5324c091b4d64707a3f41
ejb <mail@elliotbentley.com>
2014-01-18 17:38:04 +0000

    Refactored to single object

--- src/script.js
+++ src/script.js
@@ -1,75 +1,93 @@
 var mpb = {};
 
-var refreshIntervalId;
-var mDown = false;
+mpb.mouseEventRefresh;
+mpb.mouseDown = false;
 
 mpb.init = function( options ){
     this._media = options.media;
     this._bar = options.bar;
+    this._text = options.text;
     mpb.initPlayer(this._media);
     mpb.setUpProgressBar();
 }
 
-mpb.initPlayer = function(audioPlayer) {
-    audioPlayer.addEventListener('timeupdate', updateProgress, false);
-    audioPlayer.addEventListener('timeupdate', updateTimeCount, false);
-    audioPlayer.addEventListener('loadedmetadata', updateTimeCount, false);
-    updateTimeCount(audioPlayer);
-    return audioPlayer;
+mpb.initPlayer = function() {
+    this._media.addEventListener('timeupdate', mpb.updateProgress, false);
+    this._media.addEventListener('timeupdate', mpb.updateTimeCount, false);
+    this._media.addEventListener('loadedmetadata', function(){
+        mpb.updateTimeCount();
+        mpb.addClickEvents();
+    }, false);
+    this.updateTimeCount(this._media);
 }
 
 mpb.setUpProgressBar = function(){
+    var text = document.createElement('span');
+    text.textContent = this._text;
+    this._bar.style.position = "relative";
+    this._bar.style.zIndex = 1;
+    
     var progress = document.createElement('div');
     progress.id = "progress";
     progress.style.width = "0%";
+    progress.style.position = "absolute";
+    progress.style.top = 0;
+    progress.style.zIndex = -1;
+    
+    this._bar.style.webkitUserSelect = "none";
+    this._bar.style.userSelect = "none";
+    this._bar.appendChild ( text );
     this._bar.appendChild( progress );
 }
 
-
-
-function updateProgress() {
-    updateTimeCount(audio);
+mpb.updateProgress = function() {
+    mpb.updateTimeCount();
     var value = 0;
     if (audio.currentTime > 0) {
         value = Math.floor((100 / audio.duration) * audio.currentTime);
     }
-    progress.style.width = value + "%";
+    mpb._bar.childNodes[2].style.width = value + "%";
 }
 
-function updateTimeCount(){
-    var media = audio;
+mpb.updateTimeCount = function(){
     var el = document.getElementById('testdiv');
-    var currTime = Math.floor( media.currentTime );
-    var totalTime = Math.floor( media.duration );
+    var currTime = Math.floor( mpb._media.currentTime );
+    var totalTime = Math.floor( mpb._media.duration );
     el.innerHTML = currTime + "/" + totalTime;
 }
 
-function timeFromCursorPosition(element, event, duration){
+
+mpb.timeFromCursorPosition = function(element, event, duration){
     var dimensions = element.getBoundingClientRect();
     var pixelsOfBar = event.clientX - dimensions.left;
     var percentToSecs = pixelsOfBar / dimensions.width;
     return percentToSecs * duration;
 }
 
-function setProgressBar(event){
-    audio.currentTime = timeFromCursorPosition(
-        mpb._bar,event,audio.duration
+mpb.setProgressBar = function(event){
+    mpb._media;
+    mpb._media.currentTime = mpb.timeFromCursorPosition(
+        mpb._bar,
+        event,
+        audio.duration
     );
-    updateProgress();
+    mpb.updateProgress();
     
 }
 
+mpb.addClickEvents = function(){
+    bar.addEventListener("mousedown", function(ev) {
+        mpb.mouseDown = true;
+        mpb.setProgressBar(ev);
+    });
+    bar.addEventListener("mouseup", function() {
+        clearInterval(mpb.mouseEventRefresh);
+        mpb.mouseDown = false;
+    });
+    bar.addEventListener("mousemove", function(e) {
+        if ( mpb.mouseDown === true ) {
+            mpb.mouseEventRefresh = setInterval( mpb.setProgressBar(e) , 1000 );   
+        }
+    }); 
+}
 
-bar.addEventListener("mousedown", function(ev) {
-    mDown = true;
-    setProgressBar(ev);
-});
-bar.addEventListener("mouseup", function() {
-    clearInterval(refreshIntervalId);
-    mDown = false;
-});
-bar.addEventListener("mousemove", function(e) {
-    if ( mDown === true ) {
-        refreshIntervalId = setInterval( setProgressBar(e) , 1000 );   
-    }
-});
